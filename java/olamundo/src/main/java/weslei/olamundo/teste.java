9fda45837e5094d07b025b7a9e530e08b1c48667
weslei <wesleivisentin@hotmail.com>
2021-10-02 15:14:27 -0300

    3
    3

--- java/olamundo/src/main/java/weslei/olamundo/teste.java
+++ java/olamundo/src/main/java/weslei/olamundo/teste.java
@@ -3,5 +3,11 @@ package weslei.olamundo;
 public class teste {
     public static void main(String[] args) {
         System.out.println("olá, mundo");
+     
+     Integer idade = new Integer(3);
+     
+        float[] sal = new float(1825.54);
+     
+        
     }
 }  
