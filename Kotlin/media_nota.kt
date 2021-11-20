e659f6e491bfca4b5c0656402ba9f7e0f9fcf8d3
weslei <wesleivisentin@hotmail.com>
2021-11-20 17:15:51 -0300

    Update media_nota.kt

--- Kotlin/media_nota.kt
+++ Kotlin/media_nota.kt
@@ -4,12 +4,12 @@ val grade = (0..10).random()
 
 fun int.getStudentStats():String{
     println("nota $this")
-}
+
     return when(this){
         in 0..4 -> "Reprovado"
         in 5..7 -> "Mediano"
         in 8..9 -> "Bom"
         10 -> "Exelente"
         else -> "Indefinido"
-
-    }
+}
+}
