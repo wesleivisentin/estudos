590b151372d013796e91c7b042972a667d87a332
weslei <wesleivisentin@hotmail.com>
2022-01-14 20:55:05 -0300

    Update testes.js

--- onebit/Javascript/testes.js
+++ onebit/Javascript/testes.js
@@ -12,3 +12,12 @@ let spaceship = "ola"
 console.log(spaceship)
 spaceship = "ahammm"
 console.log(spaceship)
+
+
+//const
+
+const squad = "Estelar"
+console.log(squad)
+squad = "Espacial" //gera erro
+console.log(squad)
+
