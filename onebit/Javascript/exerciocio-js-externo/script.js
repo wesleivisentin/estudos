0449f3fa42baa355d365fe8134cb877c2f8ca69c
weslei <wesleivisentin@hotmail.com>
2022-01-15 19:18:41 -0300

    2

---
+++ onebit/Javascript/exerciocio-js-externo/script.js
@@ -0,0 +1,6 @@
+let oldName = prompt("qual nome da pessoa mais velha? ")
+let oldAge = prompt ("qual a idade da pessoa mais velha? ")
+let newName = prompt("qual nome da pessoa mais nova? ")
+let newAge = prompt ("qual a idade da pessoa mais nova? ")
+oldAge - newAge
+alert(oldName + " de" + oldAge + " anos, é a pessoa mais velha/n" + newName)
