2cfc5e4de70c3607a7de7df6656ba08a89eb657c
weslei <wesleivisentin@hotmail.com>
2022-01-15 19:29:52 -0300

    Update script.js

--- onebit/Javascript/exerciocio-js-externo/script.js
+++ onebit/Javascript/exerciocio-js-externo/script.js
@@ -1,7 +1,10 @@
 let oldName = prompt("qual nome da pessoa mais velha? ")
 let oldAge = prompt ("qual a idade da pessoa mais velha? ")
+
 let newName = prompt("qual nome da pessoa mais nova? ")
 let newAge = prompt ("qual a idade da pessoa mais nova? ")
+
 let difference = (oldAge - newAge)
+
 alert(oldName + " de " + oldAge + " anos, é a pessoa mais velha. \n" + newName + " de " + newAge + " anos, é a pessoa mais nova. \n"
 + "elas tem " + difference + " anos de diferença.")
