925fdb43ddf0362c8249d353a15561cd1e167afb
weslei <wesleivisentin@hotmail.com>
2022-01-18 20:58:49 -0300

    2

---
+++ onebit/Javascript/Exercício_Utilizando_condicionais/script.js
@@ -0,0 +1,11 @@
+let namePilot = prompt("Qual o seu nome piloto?")
+
+let velocityStart = 0
+
+let velocityOption = prompt ("a qual velocidade gostaria de acelerar?")
+
+let confirmation = confirm("tem certeza que quer andar a " + velocityOption + " ?")
+
+if (confirm = true) {
+    velocityStart = velocityOption
+}
