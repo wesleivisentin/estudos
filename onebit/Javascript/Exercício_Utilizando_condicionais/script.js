8258b4489dfe30c16d379a85680c8a9e54bd492c
weslei <wesleivisentin@hotmail.com>
2022-01-18 21:29:41 -0300

    Update script.js

--- onebit/Javascript/Exercício_Utilizando_condicionais/script.js
+++ onebit/Javascript/Exercício_Utilizando_condicionais/script.js
@@ -4,7 +4,7 @@ let velocityStart = 0
 
 let velocityOption = prompt ("a qual velocidade gostaria de acelerar?")
 
-let confirmation = confirm("tem certeza que quer andar a " + velocityOption + " ?")
+let confirmation = confirm("tem certeza que quer acelerar para " + velocityOption + " km/s?")
 
 
 if (confirmation) {
@@ -13,4 +13,15 @@ if (confirmation) {
 }
    
 
-alert ("é " + velocityStart)
+alert ("subindo velocidade para " + velocityStart + " km/s")
+
+//diferentes tipos de velocidades
+
+if (velocityStart <= 0){
+    alert("nave está parada. Considere partir e aumnetar a velocidade")
+}
+else {
+    if (velocityStart < 40){
+        alert("Voce es´ta devagar, podemos aumentar mais")
+    }
+}
