f1100410eb6252dedbb2e9aef383fb280da6d0b8
weslei <wesleivisentin@hotmail.com>
2022-01-19 17:05:49 -0300

    Update script.js

--- onebit/Javascript/estruturas de repetição/exercicio contando dobras com while/script.js
+++ onebit/Javascript/estruturas de repetição/exercicio contando dobras com while/script.js
@@ -1,3 +1,14 @@
 let nameNave = prompt("qual o nome da sua nave?")
 
-let enterDouble = prompt("Deseja entrar em dobra espacial?" )
+let enterDouble = confirm("Deseja entrar em dobra espacial?" )
+let doubles = 0
+if (enterDouble){
+    doubles + 1
+}
+let confirmDouble
+while(confirmDouble = true){
+  confirmDouble = confirm("deseja realizar a proxima dobra?")
+  doubles += 1
+}
+
+alert("nome da nave: " + nameNave + "\nNúmero de dobras:  " + doubles)
