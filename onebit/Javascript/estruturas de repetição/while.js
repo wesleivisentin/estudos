edb3b3fe4a5e5c1643b29a583729d93de9ba84e4
weslei <wesleivisentin@hotmail.com>
2022-01-19 16:34:21 -0300

    Update while.js

--- onebit/Javascript/estruturas de repetição/while.js
+++ onebit/Javascript/estruturas de repetição/while.js
@@ -1,7 +1,22 @@
+
 let velocity = 50
 let acceleration = 5
 
 while(velocity <= 100 ) {
     console.log("Acelerando: estamos a " + velocity + "km/s")
     velocity += acceleration //msm q: velocity = velocity + acceleration
+}
+
+
+//______
+
+let constellation = "Andromeda"
+let pos = 0
+let constellationLength = constellation.length
+
+while(pos < constellationLength){
+    if (constellation[pos] == "a" || constellation[pos] == "A"){
+        console.log(pos)
+    }
+    pos += 1
 }
