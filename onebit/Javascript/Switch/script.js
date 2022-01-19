6f42b1e59390fef6b3aa49e1ef9c0076218a2d25
weslei <wesleivisentin@hotmail.com>
2022-01-19 14:10:00 -0300

    Update script.js

---
+++ onebit/Javascript/Switch/script.js
@@ -0,0 +1,12 @@
+let velocity = 80
+
+switch(velocity * 2){
+    case 100:
+        console.log("sua velocidade é de 100 km/s")
+        break
+    case 160:
+        console.log("sua velocidade é 160 km/s")
+        break
+    default:
+        console.log("velocidade não identificada")
+}
