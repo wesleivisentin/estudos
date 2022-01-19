ef150b5eaaab9ae4f5200351f56ddfceabbd5ae8
weslei <wesleivisentin@hotmail.com>
2022-01-19 11:02:01 -0300

    Update script.js

--- onebit/Javascript/exercicio_booleans_aula_87/script.js
+++ onebit/Javascript/exercicio_booleans_aula_87/script.js
@@ -1,2 +1,6 @@
 let departureDateEntry = prompt ("digite a data de partida (formato DD/MM/YYYY )")
 let departureDate = moment(departureDateEntry, "DD/MM/YYYY")
+
+let today = moment()
+
+let datediff = today - departureDate
