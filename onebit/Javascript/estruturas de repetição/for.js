42ec673ea51cffea6552e1758006961458ab1b98
weslei <wesleivisentin@hotmail.com>
2022-01-19 17:39:40 -0300

    Update for.js

--- onebit/Javascript/estruturas de repetição/for.js
+++ onebit/Javascript/estruturas de repetição/for.js
@@ -1,6 +1,12 @@
+//trocar uma letra por outra coisa ou outra letra
 let spaceship = "helmet"
 let newSpaceship = ""
 
 for(let i = 0; i < spaceship.length; i++) {
-    console.log(spaceship[i])
+    if(spaceship[i] == "e") {
+        newSpaceship += "*"
+    } else {
+        newSpaceship += spaceship[i]
+    }
 }
+console.log(newSpaceship)
