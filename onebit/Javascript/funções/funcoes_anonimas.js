7a7d6cb493b6935b2a2bb249473061b172729c3f
weslei <wesleivisentin@hotmail.com>
2022-01-21 18:29:58 -0300

    2

---
+++ onebit/Javascript/funções/funcoes_anonimas.js
@@ -0,0 +1,6 @@
+let doubleSpeed = function(velocity){
+    return velocity * 2
+}
+let newVelocity = doubleSpeed(40)
+
+console.log(newVelocity)
---
+++ onebit/Javascript/funções/scopos.js
@@ -0,0 +1,26 @@
+//>>>>>>>>>>   escopo global   <<<<<<<<<<<<
+let spaceshipName = "supernova"//poder ser usado até dentro de uma função
+
+function changeSpaceshipName(){
+    spaceshipName = "Elemental"
+}
+
+changeSpaceshipName()//a funçao trocou o valor da variavel 
+
+
+
+
+//>>>>>>>>>>   escopo local   <<<<<<<<<<<<
+
+function startSpaceshipVelocity(){
+    let spaceshipVelocity = 0 //variavel  criada na funçao, só funciona na função
+}
+
+let spaceshipVelocity = 200
+
+startSpaceshipVelocity()
+
+
+
+
+
