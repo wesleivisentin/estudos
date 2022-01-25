ff750f52d38081495f7a04b8a11618717e0eeb02
weslei <wesleivisentin@hotmail.com>
2022-01-25 19:13:25 -0300

    Update indrodução.js

--- onebit/Javascript/Arrays/indrodução.js
+++ onebit/Javascript/Arrays/indrodução.js
@@ -2,7 +2,18 @@ let hitcheadSpaceships = ["Supernova", "Elemental", "Helmet"]
 
 console.log(hitcheadSpaceships[0])
 
+//__________________________________________
 
 let spaceshiptInfo = ["colossus", 7, true]
 
-console.log = (spaceshiptInfo[1])
+console.log(spaceshiptInfo[1])
+
+//_________________________________________________________
+
+let hitcheadSpaceshipss = new Array ("supernova", "Elemental", "Helmet", "Colossus")
+
+console.log(hitcheadSpaceshipss)
+
+//-----------------------------------------------------------
+let number = new Array (5) //ele vai criar 5 posiçoes de array..e não um array com o nume 5!!
+console.log(number)
