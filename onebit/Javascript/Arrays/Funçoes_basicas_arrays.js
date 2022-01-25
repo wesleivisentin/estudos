cd045a1639bd208cd0e8296a44861e89785c5069
weslei <wesleivisentin@hotmail.com>
2022-01-25 20:16:27 -0300

    Update Funçoes_basicas_arrays.js

--- onebit/Javascript/Arrays/Funçoes_basicas_arrays.js
+++ onebit/Javascript/Arrays/Funçoes_basicas_arrays.js
@@ -3,6 +3,7 @@ let naves = ["Colossus", "Elemental", "Helmet"]
 console.log(naves)
 
 naves.push("Supernova") // adiciona um item na lista
+naves.unshift("fenix") // adiciona elemento no começo
 
 console.log(naves)
 
@@ -14,3 +15,11 @@ console.log(removeNave)
 
 let removeNave2 = naves.shift() //remove 1 item da lista 
 console.log(removeNave2)
+
+console.log(naves.length)// imprime tamanho da array
+
+
+//imprimir a localização de um elemento
+
+let localElemento = naves.indexOf("Elemental")
+console.log(localElemento)
