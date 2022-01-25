38c0518b250ca865544fcd3ce7f88d30e5a0e1ad
weslei <wesleivisentin@hotmail.com>
2022-01-25 20:04:30 -0300

    Create Funçoes_basicas_arrays.js

---
+++ onebit/Javascript/Arrays/Funçoes_basicas_arrays.js
@@ -0,0 +1,9 @@
+let naves = ["Colossus", "Elemental", "Helmet"]
+
+console.log(naves)
+
+naves.push("Supernova") // adiciona um item na lista
+
+console.log(naves)
+
+naves.pop() //retira ultimo elemente se não especificado
