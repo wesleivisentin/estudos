788ba1bce35cc9e466c70115786d72022e7a1094
weslei <wesleivisentin@hotmail.com>
2022-01-25 19:05:56 -0300

    Update indrodução.js

--- onebit/Javascript/Arrays/indrodução.js
+++ onebit/Javascript/Arrays/indrodução.js
@@ -1,3 +1,8 @@
 let hitcheadSpaceships = ["Supernova", "Elemental", "Helmet"]
 
 console.log(hitcheadSpaceships[0])
+
+
+let spaceshiptInfo = ["colossus", 7, true]
+
+console.log = (spaceshiptInfo[1])
