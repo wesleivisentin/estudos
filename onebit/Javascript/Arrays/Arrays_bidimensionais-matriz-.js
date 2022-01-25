0f0f0bfbc93699b1c97b77574ad5dcda5df88f43
weslei <wesleivisentin@hotmail.com>
2022-01-25 20:38:04 -0300

    arrays bi

---
+++ onebit/Javascript/Arrays/Arrays_bidimensionais-matriz-.js
@@ -0,0 +1,6 @@
+let hitchSpaceships = [["Elemental", 7], ["Helmet", 13], ["Supernova", 5], ["Supernova", 10]]
+
+hitchSpaceships.push(["Colossus", 8])
+console.log(hitchSpaceships)
+console.log(hitchSpaceships[3])
+console.log(hitchSpaceships[3] [0])
