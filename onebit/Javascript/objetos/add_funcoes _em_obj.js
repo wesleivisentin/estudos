de867011dd7fba533a03ad29fd487b037eeda6be
weslei <wesleivisentin@hotmail.com>
2022-01-29 17:26:41 -0300

    Update add_funcoes _em_obj.js

--- onebit/Javascript/objetos/add_funcoes _em_obj.js
+++ onebit/Javascript/objetos/add_funcoes _em_obj.js
@@ -5,7 +5,27 @@ let spaceship = {
     type: "extração",
     maxCrew: 20,
     turnON: function() {
-        alert("preparando propulção")
-        alert("Ligando computador de bordo")
+        console.log("preparando propulção")
+        console.log("Ligando computador de bordo")
     }
 }
+
+//chamando a função
+
+spaceship.turnON()
+
+//add mais uma propriedade:
+spaceship.velocity = 0
+
+//add mais uma funçao:
+spaceship.outraFuncao = function(acceleration) {
+    this.velocity += acceleration
+}
+console.log(spaceship)
+
+spaceship.outraFuncao(10)
+
+console.log(spaceship)
+
+
+//this para referenciar o proprio objeto
