cd8712d68835006315a78be12cc86a6584e8d62b
weslei <wesleivisentin@hotmail.com>
2022-01-29 16:42:11 -0300

    Create add_funcoes _em_obj.js

---
+++ onebit/Javascript/objetos/add_funcoes _em_obj.js
@@ -0,0 +1,11 @@
+//quando a propriedade tem uma funçao...éla é chamada de metodo
+
+let spaceship = {
+    name: "demeter",
+    type: "extração",
+    maxCrew: 20,
+    turnON: function() {
+        alert("preparando propulção")
+        alert("Ligando computador de bordo")
+    }
+}
