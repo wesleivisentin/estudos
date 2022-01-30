1a1b81da860107c432ca49b34529216ebf085cc8
weslei <wesleivisentin@hotmail.com>
2022-01-29 21:34:36 -0300

    2

---
+++ onebit/Javascript/objetos/exercicio/feito_pelo_professor.js
@@ -0,0 +1,50 @@
+let spaceship = {
+    velocity: 0,
+    speedUp: function(acceleration) {
+        this.velocity += acceleration
+    }
+}
+
+function registerSpaceship() {
+    spaceship.name = prompt("Informe o nome da nave")
+    spaceship.type = prompt("Informe o tipo da nave")
+    spaceship.maxVelocity = Number(prompt("Informe a velocidade mãxima da nave (km/s)"))
+}
+
+function acelerate() {
+    let acceleration = Number(prompt("Quanto você quer acelerar? (km/s)"))
+    spaceship.speedUp(acceleration)
+    if(spaceship.velocity > spaceship.maxVelocity) {
+        alert(  "VELOCIDADE MÁXIMA ULTRAPASSADA!" +
+                "\nVelocidade da Nave: " + spaceship.velocity + "km/s" + 
+                "\nVelocidade máxima da Nave: " + spaceship.maxVelocity + "km/s")
+    }
+}
+
+function stop() {
+    alert(  "Nome: " + spaceship.name + "\nTipo: " + spaceship.type + "\nVelocidade da nave: " 
+            + spaceship.velocity + "\nMáxima da Nave: " + spaceship.maxVelocity)
+    spaceship.velocity = 0
+}
+
+function showMenu() {
+    let chosenOption
+    do {
+        chosenOption = prompt("Você deseja:\n1- Acelerar\n2- Parar")
+        switch(chosenOption) {
+            case "1":
+                acelerate()
+                break
+            case "2":
+                stop()
+                break
+            default:
+                alert("Opação inválida")
+        }
+    } while(chosenOption != "2")
+}
+
+registerSpaceship()
+showMenu()
+
+
--- onebit/Javascript/objetos/exercicio/script.js
+++ onebit/Javascript/objetos/exercicio/script.js
@@ -4,7 +4,7 @@ let velocityMaxSpaceship = prompt ("Qual e velocidade Maxima da sua nave?")
 let velocityNow = 0
 
 function newVelocity() {
-    velocityNow = velocityNow += number(acelerattion)
+    velocityNow = velocityNow += parseInt(acelerattion)
     if(velocityNow > velocityMaxSpaceship){
         alert("ATENÇÃO!!!\nVelocidade Máxima de " + velocityMaxSpaceship + "km/s atingida")
         alert("Parada de emergencia ativada")
@@ -13,7 +13,7 @@ function newVelocity() {
     }
     
 }
- 
+
 let nave = {
     name: nameSpaceship,
     type: typeSpaceship,
@@ -43,6 +43,6 @@ else {
 }
 }
 
-alert("nome da nave: " + nave.name + "\nTipo da nave: " + nave.type + "\nVelocidade máxima: " + nave.velocity + "km/s" + "\nÚltima velocidade registrada: " + velocityNow + "km/s")
+alert("nome da nave: " + nave.name + "\nTipo da nave: " + nave.type + "\nVelocidade máxima: "+ nave.velocity + "km/s" + "\nÚltima velocidade registrada: " + velocityNow + "km/s")
 
 
