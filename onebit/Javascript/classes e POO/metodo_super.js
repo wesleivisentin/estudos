27fec0d2a4832e7c46cea0f760813dcbda46b96a
weslei <wesleivisentin@hotmail.com>
2022-02-03 20:50:20 -0300

    Update metodo_super.js

--- onebit/Javascript/classes e POO/metodo_super.js
+++ onebit/Javascript/classes e POO/metodo_super.js
@@ -1,15 +1,22 @@
 class Spaceship{
-    constructor(name, maxCrew, maxRecommendedVelocity) {
+    constructor(name, maxCrew, maxRecommendedValue) {
         this.name = name
         this.maxCrew = maxCrew
-        this.maxRecommendedVelocity = maxRecommendedVelocity
+        this.maxRecommendedValue = maxRecommendedValue
         this.currentVelocity = 0
     }
     speedUp(accleration){
         this.currentVelocity += accleration
-        if(this.currentVelocity > this.maxRecommendedVelocity) {
+        if(this.currentVelocity > this.maxRecommendedValue) {
             alert("Velocidade máxima ultrapassada!\nDiminua ou poderá provocar danos à nave.")
         }
 
+    }
+}
+
+class TransportSpaceship extends Spaceship{
+    constructor(name, maxCrew, masRecommendedValue, maxLoadWeight){
+    super(name, maxCrew, masRecommendedValue) // chama o constructor do Spaceship
+    this.maxLoadWeight = maxLoadWeight
     }
 }
