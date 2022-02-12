177672e9b788275d57360ec8a8b3e75e6dd97348
weslei <wesleivisentin@hotmail.com>
2022-02-12 18:44:47 -0300

    n feito por mim
    n feito por mim

---
+++ onebit/Javascript/classes e POO/exercicio 2/transport_spaceship.js
@@ -0,0 +1,8 @@
+class TransportSpaceship extends Spaceship {
+    constructor(name, crewQuantity, sitsQuantity) {
+      super(name, crewQuantity)
+      this.sitsQuantity = sitsQuantity
+    }
+  }
+  
+  
