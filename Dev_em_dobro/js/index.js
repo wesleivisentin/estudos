eb11c9c37fcd42d0bd36c513fc73cb1079cdfa7b
weslei <wesleivisentin@hotmail.com>
2022-03-21 20:10:55 -0300

    3

---
+++ Dev_em_dobro/js/index.js
@@ -0,0 +1,12 @@
+/*
+Quando clicar no pokemon da listagem temos que esconder o cartao do pokemon aberto e mostrar o cartao correspondente ao que foi selecionado na listagem
+
+pra isso vamos precisar trabralhar com dois elementos 
+1 - listagem
+2 - cartão pokémon
+
+
+*/
+
+const nome = "Weslei"
+console.log(nome)
