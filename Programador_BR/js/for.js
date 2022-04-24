ad7274cd00f38567207130d34f6a89d61f92e5ac
weslei <wesleivisentin@hotmail.com>
2022-04-23 22:34:50 -0300

    Update for.js

--- Programador_BR/js/for.js
+++ Programador_BR/js/for.js
@@ -15,3 +15,8 @@
  for (var i in alunos) {  // in poe o valor index de alunos dentro do i
     console.log(alunos[i])
  }
+
+
+ for (var aluno of alunos) {  // of pega os elementos da array em vez do index como o in faz
+    console.log(aluno)
+ }
