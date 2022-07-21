84448c7d67c85b3f8db4b044551e9532998964b7
weslei <wesleivisentin@hotmail.com>
2022-07-21 20:36:43 -0300

    Update criar_usuario.js

--- Programador_BR/Firebase/integrando_fire_base/criar_usuario.js
+++ Programador_BR/Firebase/integrando_fire_base/criar_usuario.js
@@ -9,21 +9,62 @@ var config = {
     messagingSenderId: "360124705394",
     appId: "1:360124705394:web:f34b4cec4b348b996270eb",
     measurementId: "G-8669S842FP"
-  };
+};
 
-  FirebaseError.initializeApp(config)
-  var db = firebase.firestore()
+FirebaseError.initializeApp(config)
+var db = firebase.firestore()
+
+let auth = firebase.auth()
+function criarUsuario() {
+    let newUserEmail = "novoteste@teste.com"
+    let newUserPassword = "abc123"
 
 
 
-  let newUserEmail = "novoteste@teste.com"
-  let newUserPassword = "abc123"
+    auth.createUserWithEmailAndPassword(newUserEmail, newUserPassword)
+        .then(user => {
+            console.log(user);
+        }).catch(error => {
+            console.log(error);
+        })
+}
+//ver se esta logado
 
-  let auth = firebase.auth()
+function login() {
 
-  auth.createUserWithEmailAndPassword(newUserEmail, newUserPassword)
-    .then(user=> {
+    let userEmail = "novoteste@teste.com"
+    let userPassword = "abc123"
+
+    auth.signInWithEmailAndPassword(userEmail, userPassword)
+        .then(loggedUser => {
+            console.log(loggedUser);
+        }).catch(error => {
+            console.log(error);
+        })
+
+    let user = auth.currentUser
+
+    console.log(user);
+
+}
+
+
+//verificar sem tem alguem logado - deslogar
+
+auth.onAuthStateChanged(user => {
+    if (user) {
         console.log(user);
+    } else {
+        console.log("ninguem logado");
+    }
+})
+
+function logout() {
+    auth.signOut().then(obj => {
+        console.log(obj);
     }).catch(error => {
         console.log(error);
     })
+}
+
+setTimeout(login, 2000)
