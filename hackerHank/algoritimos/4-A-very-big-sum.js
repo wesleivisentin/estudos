f40e3ec5e9b7df83134238bdb35d41bc800c841b
weslei <wesleivisentin@hotmail.com>
2023-11-23 17:23:16 -0300

    att 4 exercicio

---
+++ hackerHank/algoritimos/4-A-very-big-sum.js
@@ -0,0 +1,11 @@
+function aVeryBigSum(ar) {
+    // Write your code here
+    let total = 0
+    for (let i = 0; i < ar.length; i++) {
+        
+        total += ar[i]
+        
+    }
+    return total
+
+}
